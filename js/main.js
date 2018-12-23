bc13f75aa2236b7ef1fe185b7a414c2d19237539
vivekvj <vivek.vijay@litmus7.com>
2018-12-23 10:07:36 +0530

    design changes

--- js/main.js
+++ js/main.js
@@ -6,3 +6,34 @@ jQuery(document).ready(function($){
 		return false;
 	});
 	});
+
+	(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.scroll-trigger').click(function() {
+    $('.mob-menu').removeClass('active');
+		$('.header').removeClass('open');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  // $('body').scrollspy({
+  //   target: '#mainNav',
+  //   offset: 54
+  // });
+
+})(jQuery);
